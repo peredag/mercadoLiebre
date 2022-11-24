ebff59a9b784f87870624b32e0a4a0fe8f425903
unknown <greta.pereda@gmail.com>
2022-11-23 21:48:35 -0400

    agrego vistas register, login y css

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.get('/', function (req, res){
     res.sendFile(path.join(__dirname, './views/home.html'))
 })
 
+app.get('/register', function (req, res){
+    res.sendFile(path.join(__dirname, './views/register.html'))
+})
+
+app.get('/login', function (req, res){
+    res.sendFile(path.join(__dirname, './views/login.html'))
+})
+
 
 // levanto serv-------------
 app.listen(PORT, () => console.log(` 
